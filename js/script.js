fc695605fc172eec7c00c7d5b70e9bba4635f6a6
Simone Tonti <simone.tonti98@gmail.com>
2023-12-04 13:04:10 +0100

    Commit ore 13

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log(window)
+let c=0
+const SearchBar = function ()
+{
+    const icon = document.querySelector('SearchButton')
+    const form = document.querySelector('input')
+    if((c%2)===0)
+    {
+        form.style.display='inline';
+    }
+    else
+    {
+        form.style.display='none';
+    }
+    c++
+}
